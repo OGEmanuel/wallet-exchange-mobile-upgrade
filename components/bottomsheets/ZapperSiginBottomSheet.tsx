2aa287d278437b794844be076cae7804f3c12910
Busola <busola@zap.africa>
2025-09-26 07:40:46 +0100

    refactor: comment out LoginToZap component in ZapperSiginBottomSheet
    - Removed the LoginToZap component from the ZapperSiginBottomSheet to streamline the onboarding process.
    - Updated the code to comment out the component instead of deleting it, preserving the option for future use.

--- components/bottomsheets/ZapperSiginBottomSheet.tsx
+++ components/bottomsheets/ZapperSiginBottomSheet.tsx
@@ -5,7 +5,6 @@ import { useTheme } from "@shopify/restyle";
 import React, { useEffect, useRef } from "react";
 import { Image, StyleSheet, View } from "react-native";
 import { Confetti, ConfettiMethods } from "react-native-fast-confetti";
-import LoginToZap from "../onboarding/LoginToZap";
 import VerifyYourIdentity from "../onboarding/VerifyYourIdentity";
 import AnimatedGradientBottomSheet, {
   AnimatedGradientBottomSheetRef,
@@ -48,7 +47,7 @@ export default function ZapperSiginBottomSheet({
           style={{ height: 40, width: 120, alignSelf: "center", marginTop: 16 }}
           resizeMode="contain"
         />
-        <LoginToZap />
+        {/* <LoginToZap /> */}
         {/* <EmailVerification /> */}
         {/* <EnterUsername /> */}
         {/* <UsernameSuccess confettiRef={confettiRef} /> */}
