20808f2215337277dc3af6165bb7a1e5eeb5aad0
Omotayo Eyimofe <eyimofeomotayo@gmail.com>
2025-09-28 23:25:52 +0100

    feat: enhance DocumentCapture component with button functionality
    - Updated the `DocumentCapture` component to include a container style for improved layout.
    - Added two `CustomButton` components for "Take a photo" and "Upload photo" actions, enhancing user interaction.
    - Adjusted styles to position buttons appropriately within the component.

--- components/kyc/DocumentCapure.tsx
+++ components/kyc/DocumentCapure.tsx
@@ -4,12 +4,12 @@ import { SCREEN_HEIGHT, SCREEN_WIDTH } from "@gorhom/bottom-sheet";
 import { useTheme } from "@shopify/restyle";
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
-import { CustomText } from "../general";
+import { CustomButton, CustomText } from "../general";
 
 export default function DocumentCapure() {
   const theme = useTheme<Theme>();
   return (
-    <View>
+    <View style={styles.container}>
       <CustomText variant="header" style={styles.title}>
         National ID
       </CustomText>
@@ -36,10 +36,43 @@ export default function DocumentCapure() {
           style={{ width: 16, height: 20 }}
           resizeMode="contain"
         />
-        <CustomText variant="body" style={[styles.subtitle, { fontSize: 12 }]}>
+        <CustomText
+          variant="body"
+          style={[styles.subtitle, { fontSize: 12, opacity: 1 }]}
+        >
           I consent to Zap collecting, processing and sharing my information for
           KYC purposes as stated in the policy
         </CustomText>
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          text="Take a photo"
+          onPress={() => {}}
+          width="100%"
+          height={56}
+          borderRadius={56}
+          bgColor={theme.colors.primaryColor}
+          color={theme.colors.white}
+          variant="bodySubheader"
+          fontSize={14}
+          disabled={false}
+          disabledColor={theme.colors.borderColor}
+        />
+        <CustomButton
+          text="Upload photo"
+          onPress={() => {}}
+          width="100%"
+          height={56}
+          borderRadius={56}
+          bgColor={theme.colors.mainBackgroundColor}
+          color={theme.colors.white}
+          variant="bodySubheader"
+          fontSize={14}
+          disabled={false}
+          disabledColor={theme.colors.borderColor}
+          borderWidth={1}
+          borderColor={theme.colors.borderColor}
+        />
       </View>
     </View>
   );
@@ -86,5 +119,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 16,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 150,
+    width: SCREEN_WIDTH * 0.9,
+    alignSelf: "center",
+    gap: 16,
   },
 });
