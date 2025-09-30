f19db46fbaa2da415d9fc4cbde6e757bfce94e68
Neon <danielemmanuel257@gmail.com>
2025-09-30 12:23:13 +0100

    feat: add help & support, about, and profile sections with new components
    feat(help): add help & support section with chat and tutorials
    feat(about): implement about page with external links
    feat(profile): add 2FA enable functionality
    feat(sidebar): add markets link and change pin functionality
    refactor: update back navigation across multiple screens
    style: improve ThemedExternalLinkIcon component
    chore: add new dependencies for chat and UI components

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,5 +1,6 @@
 import {
   ThemedBookIcon,
+  ThemedChartIcon,
   ThemedFaceIDIcon,
   ThemedGiftFill3Icon,
   ThemedHeatIcon,
@@ -9,6 +10,7 @@ import {
   ThemedShieldFillIcon,
   ThemedStarFillIcon,
 } from "@/assets/svg/wallet-icons-components";
+import useBottomSheetRefs from "@/hooks/useBottomSheetRefs";
 import {
   selectWalletConnected,
   setWalletConnected,
@@ -23,6 +25,7 @@ import React from "react";
 import { Image, Pressable } from "react-native";
 import { ScrollView, Switch } from "react-native-gesture-handler";
 import { useDispatch, useSelector } from "react-redux";
+import ChangePinBottomSheet from "../bottomsheets/preference/ChangePinBottomSheet";
 import Box from "../general/Box";
 import CustomText from "../general/CustomText";
 import SidebarItemCard from "./SidebarItemCard";
@@ -31,6 +34,7 @@ const Sidebar = () => {
   const isConnect = useSelector(selectWalletConnected);
   const dispatch = useDispatch();
   const theme = useTheme<Theme>();
+  const { changePinRef } = useBottomSheetRefs();
 
   const handleConnect = () => {
     dispatch(setWalletConnected(!isConnect));
@@ -88,6 +92,19 @@ const Sidebar = () => {
       ),
       title: "Address book",
       link: "/dashboard/home/address-book",
+      isActive: false,
+    },
+    {
+      icon: (
+        <ThemedChartIcon
+          width={20}
+          height={20}
+          darkModeColor={theme.colors.bodyTextColor}
+          lightModeColor={theme.colors.bodyTextColor}
+        />
+      ),
+      title: "Markets",
+      link: "/dashboard/home/wallet-home/cards",
       isActive: false,
     },
     {
@@ -155,6 +172,7 @@ const Sidebar = () => {
       title: "Change Zap PIN",
       link: "/dashboard/home/about",
       isActive: false,
+      onPress: () => changePinRef.current?.snapToIndex(1),
     },
   ];
 
@@ -182,7 +200,7 @@ const Sidebar = () => {
         />
       ),
       title: "Help & Support",
-      link: "/dashboard/home/help",
+      link: "/dashboard/home/wallet-home/more/help",
       isActive: false,
     },
     {
@@ -195,7 +213,7 @@ const Sidebar = () => {
         />
       ),
       title: "About Zap Wallet",
-      link: "/dashboard/home/about",
+      link: "/dashboard/home/wallet-home/more/about",
       isActive: false,
     },
   ];
@@ -400,6 +418,7 @@ const Sidebar = () => {
           <LearnWithZapCards />
         </ScrollView>
       </Box> */}
+      <ChangePinBottomSheet ref={changePinRef} />
     </Box>
   );
 };
